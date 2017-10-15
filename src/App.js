12e21b833addcbf150bb9ce3b17a0969a1a4e41d
Ville <villekarjala@gmail.com>
2017-10-15 21:15:05 +0300

    first component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
+import React from 'react';
 
-const App = () => <h1>Hello</h1>
+class App extends React.Component {
+  render() {
+    return <h1>Hello world</h1>
+  }
+}
+//const App = () => <h1>Hello</h1>
 export default App;
