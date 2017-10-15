36374948f441c4423956f4be3854dd6b9426acef
Ville <villekarjala@gmail.com>
2017-10-15 23:17:47 +0300

    accessing sub data + validation added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ class App extends React.Component {
   render() {
     let txt = this.props.txt;
     return  (
-
     <div>
       <Widget update={this.update.bind(this)}/>
       <Widget update={this.update.bind(this)}/>
@@ -23,10 +22,14 @@ class App extends React.Component {
       <b>Bold</b>
       <p>{txt}</p>
       <p>{this.state.txt}</p>
+      <Button><Heart/>React</Button>
+      <Title text="the t"/>
     </div>
     )
+
   }
 }
+
 
 App.propTypes = {
   //Note React.PropTypes is deprecated
@@ -40,6 +43,27 @@ App.defaultProps = {
 
 const Widget = (properties) =>
 <input type="text" onChange={properties.update}/>
+
+const Button = (props) => <button>{props.children}</button>
+
+const Title = (props) => <h1>Title: {props.text}</h1>
+Title.propTypes = {
+  text(props,propName,component) {
+    if(!(propName in props)) {
+      return new Error('missing ${propName}')
+    }
+    if (props[propName].length < 6) {
+      return new Error(propName+' was too short')
+    }
+  }
+}
+
+
+class Heart extends React.Component {
+  render() {
+    return <span>&hearts;</span>
+  }
+}
 
 //stateless function component
 //const App = () => <h1>Hello</h1>
